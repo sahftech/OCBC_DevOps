654758a331456eb48235d9ff5041f50918c3e2a6
sahftech <sahfuan@gmail.com>
2023-11-07 11:03:36 +0800

    Change made

--- tests/UITest.test.js
+++ tests/UITest.test.js
@@ -19,13 +19,47 @@ before(async function () {
 });
 
 describe('Testing Index Screen', function () {
+
+    this.timeout(100000);
+
     it('Should show title: Banking Website', async () => {
-        await driver.get('https://localhost:5050/'); // Navigate Sauce Demo
+        await driver.get('http://localhost:5050/'); // Navigate Sauce Demo
         const title = await driver.getTitle(); // Get the title of the web page
         expect(title).to.equal("Banking Website"); // Assert that title matches "Swag Labs"
     });
 
+    it('Should login successfully', async function () {
+        const baseUrl = 'http://localhost:' + server.address().port;
+        await driver.get(baseUrl);
+    
+        // Locate and interact with the email field
+        const accessElement = await driver.findElement(By.id('access'));
+        await accessElement.click(); // Click on the element
+        await accessElement.sendKeys('john');
+    
+        // Locate and interact with the email field
+        const pinElement = await driver.findElement(By.id('pin'));
+        await pinElement.click(); // Click on the element
+        await pinElement.sendKeys('123456');
+    
+        // Locate and interact with the Login button
+        const loginButton = await driver.findElement(By.id('login'));
+        await loginButton.click();
+    
+        // Wait for the page to be redirected
+        await driver.wait(until.urlIs(baseUrl + '/home.html'), 10000);
+    
+        // Assert that the URL matches the expected URL
+        const currentUrl = await driver.getCurrentUrl();
+        expect(currentUrl).to.equal('http://localhost:' + server.address().port + '/home.html');
+    
+    });
+
+
+
 });
+
+
 
 after(async function () {
     //await driver.quit();
