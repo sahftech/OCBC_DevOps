ce3e4059539dad1833a2ac167befd5df2fa9a555
ry-se <ryans1633@gmail.com>
2023-11-16 00:44:32 +0800

    clear imports

--- tests/UITest.js
+++ tests/UITest.js
@@ -1,5 +1,5 @@
 const { app } = require('../index');
-const { Builder, By, Key, until } = require('selenium-webdriver');
+const { By, until } = require('selenium-webdriver');
 const { describe, it } = require('mocha');
 const { expect } = require('chai');
 
